09061172a6f90b661800ee08a15ea13414540235
hamzaali <hamza54834@gmail.com>
2020-02-23 11:04:09 -0800

    firebase functions fulfilments

---
+++ fulfilment agent using/functions/index.js
@@ -0,0 +1,10 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+ exports.webhook = functions.https.onRequest((request, response) => {
+   response.send({
+       speech:"Hello world response for webhook"
+   })
+});
